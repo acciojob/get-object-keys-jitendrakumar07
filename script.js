062cdc45cc9571570df974473df7fb426a33b392
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-02-20 17:33:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+const student = {
+  name: "John",
+};
+\
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Example usage:
+console.log(getKeys(student)); 
